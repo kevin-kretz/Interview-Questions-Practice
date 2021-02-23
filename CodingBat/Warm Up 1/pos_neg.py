e13153da703ef99c017270d1f387bd06711327e6
Kevin Kretz <kkretz17@gmail.com>
2021-02-23 14:22:37 -0900

    Create pos_neg.py

---
+++ CodingBat/Warm Up 1/pos_neg.py
@@ -0,0 +1,16 @@
+"""
+https://codingbat.com/prob/p162058
+
+
+Given 2 int values, return True if one is negative and one is positive.
+Except if the parameter "negative" is True, then return True only if both are negative.
+
+pos_neg(1, -1, False) → True
+pos_neg(-1, 1, False) → True
+pos_neg(-4, -5, True) → True
+"""
+def pos_neg(a, b, negative):
+  if negative:
+    return(a<0 and b<0)
+  else:
+    return((a<0) ^ (b<0))
