eebf27058fa64499256379af86737d5939d354b0
Kevin Kretz <kkretz17@gmail.com>
2021-02-23 13:42:12 -0900

    Create diff21.py

---
+++ CodingBat/Warm Up 1/diff21.py
@@ -0,0 +1,16 @@
+"""
+https://codingbat.com/prob/p197466
+
+
+Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+diff21(19) → 2
+diff21(10) → 11
+diff21(21) → 0
+"""
+
+def diff21(n):
+  if n > 21:
+    return(abs(n-21) * 2)
+  else:
+    return(abs(n-21))
