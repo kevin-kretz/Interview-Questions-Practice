4a5fe77308856d64e41469c482fda7902b9a6575
Kevin Kretz <kkretz17@gmail.com>
2021-02-23 15:45:28 -0900

    Added code

--- CodingBat/Warm Up 2/string_times.py
+++ CodingBat/Warm Up 2/string_times.py
@@ -1,1 +1,11 @@
+"""
+https://codingbat.com/prob/p193507
 
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+"""
+def string_times(str, n):
+  return(str * n)
