78bad41123ba383375884d0aaca58fd1ddb55322
Kevin Kretz <kkretz17@gmail.com>
2021-02-24 13:29:46 -0900

    Create make_abba.py

---
+++ CodingBat/String 1/make_abba.py
@@ -0,0 +1,13 @@
+"""
+https://codingbat.com/prob/p182144
+
+
+Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+make_abba('Hi', 'Bye') → 'HiByeByeHi'
+make_abba('Yo', 'Alice') → 'YoAliceAliceYo'
+make_abba('What', 'Up') → 'WhatUpUpWhat'
+"""
+
+def make_abba(a, b):
+  return(a+b+b+a)
