f44ed340a8249f67a1b7b63aecc6b8c7a8c7fd87
Kevin Kretz <kkretz17@gmail.com>
2021-02-24 11:36:06 -0900

    inserted line break in description

--- CodingBat/Warm Up 2/string_match.py
+++ CodingBat/Warm Up 2/string_match.py
@@ -1,7 +1,8 @@
 """
 https://codingbat.com/prob/p182414
 
-Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring.
+So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
 
 
 string_match('xxcaazz', 'xxbaaz') → 3
