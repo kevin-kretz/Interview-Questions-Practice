c7b35c0a880c53fad6090be20e58532e54b040b8
Kevin Kretz <kkretz17@gmail.com>
2021-03-01 13:05:13 -0900

    Create make_pi.py

---
+++ CodingBat/List 1/make_pi.py
@@ -0,0 +1,11 @@
+"""
+https://codingbat.com/prob/p113659
+
+
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+make_pi() → [3, 1, 4]
+"""
+
+def make_pi():
+  return[3, 1, 4]
