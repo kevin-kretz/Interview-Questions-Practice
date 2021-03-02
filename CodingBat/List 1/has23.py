c8bea261e67c4fca402bbffd57459c021082ec2e
Kevin Kretz <kkretz17@gmail.com>
2021-03-02 08:39:46 -0900

    Create has23.py

---
+++ CodingBat/List 1/has23.py
@@ -0,0 +1,12 @@
+"""
+https://codingbat.com/prob/p177892
+
+Given an int array length 2, return True if it contains a 2 or a 3.
+
+has23([2, 5]) → True
+has23([4, 3]) → True
+has23([4, 5]) → False
+"""
+
+def has23(nums):
+  return((2 in nums) or (3 in nums))
