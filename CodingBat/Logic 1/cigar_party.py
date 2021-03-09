39e36a740276c0dbfef7190b35b8f00cb261d0b7
Kevin Kretz <kkretz17@gmail.com>
2021-03-09 09:12:36 -0900

    Create cigar_party.py

---
+++ CodingBat/Logic 1/cigar_party.py
@@ -0,0 +1,14 @@
+"""
+https://codingbat.com/prob/p195669
+
+When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return True if the party with the given values is successful, or False otherwise.
+
+cigar_party(30, False) → False
+cigar_party(50, False) → True
+cigar_party(70, True) → True
+"""
+
+def cigar_party(cigars, is_weekend):
+  if (is_weekend):
+    return(40 <= cigars)
+  return(40 <= cigars <= 60)
